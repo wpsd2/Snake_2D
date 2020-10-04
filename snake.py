08ff06064a30303e64aa4628347d0fc1f1c5ad2f
wpsd2 <wpsd2@hotmail.com>
2020-10-04 11:28:31 -0300

    ajustano a posição da fruta

--- snake.py
+++ snake.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 
 pygame.init()
@@ -8,12 +10,16 @@ pygame.display.update()
 
 class Frutinha:
     cor = (255, 0, 0)
-    posicao = (200, 200)
+
     tamanho = (10, 10)
+    posicao = (200, 200)
 
     def __init__(self):
         self.textura = pygame.Surface(self.tamanho)
         self.textura.fill(self.cor)
+        x = random.randint(0, 49) * 10
+        y = random.randint(0, 49) * 10
+        self.posicao = (x, y)
 
 
 
